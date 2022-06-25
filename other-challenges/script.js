239aa91f9abf5e8507563176fd38204ff94650f5
wesbro <wrbrown12@gmail.com>
2022-06-25 17:36:36 -0400

    updated code

--- other-challenges/script.js
+++ other-challenges/script.js
@@ -58,7 +58,7 @@
 //   buzz
 
 function fizzBuzz(n) {
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
         if ((i % 3 === 0) && (i % 5 === 0)) {
             console.log("fizzbuzz")
         } else if (i % 5 === 0) {
